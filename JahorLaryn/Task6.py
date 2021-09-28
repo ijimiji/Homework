fec48966f37a3a642c91744751bc4a537b9ea43e
Jahor Laryn <mrojahor@gmail.com>
2021-09-28 20:47:41 +0300

    Add Task6.

---
+++ JahorLaryn/Task6.py
@@ -0,0 +1,11 @@
+class Sun(object):
+    @classmethod
+    def inst(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Sun, cls).__new__(cls)
+        return cls.instance
+
+
+s1 = Sun.inst()
+s2 = Sun.inst()
+print(s1 is s2)
